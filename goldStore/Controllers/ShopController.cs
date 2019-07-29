41559c1e782e7196c910c19aace28a107af8720d
bem deneme <bemdeneme@hotmail.com>
2019-07-29 15:03:41 +0300

    shop controler

---
+++ goldStore/Controllers/ShopController.cs
@@ -0,0 +1,46 @@
+﻿using goldStore.Areas.Panel.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace goldStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        ProductRepository repoProduct = new ProductRepository(new Areas.Panel.Models.goldstoreEntities());
+        CategoryRepository repoCategory = new CategoryRepository(new Areas.Panel.Models.goldstoreEntities());
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Products");
+        }
+        public ActionResult PartialCategory()
+        {
+            return PartialView(repoCategory.GetAll());
+
+        }
+
+        public ActionResult Products()
+
+        {
+
+            return View(repoProduct.GetAll());
+        }
+
+        public ActionResult Product(int ? categoryId)
+        {
+
+            var result = repoProduct.GetAll();
+            if(categoryId!=null)
+            {
+
+                result = result.Where(x => x.categoryId == categoryId).ToList();
+            }
+
+            return View(result);
+
+        }
+    }
+}
