6b6af62a06caa2dd566bcc5e35f7173c82215fd5
bem deneme <bemdeneme@hotmail.com>
2019-07-30 15:25:17 +0300

    detay sayfası

--- goldStore/Controllers/ShopController.cs
+++ goldStore/Controllers/ShopController.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace goldStore.Controllers
 {
@@ -11,10 +16,21 @@ namespace goldStore.Controllers
     {
         ProductRepository repoProduct = new ProductRepository(new Areas.Panel.Models.goldstoreEntities());
         CategoryRepository repoCategory = new CategoryRepository(new Areas.Panel.Models.goldstoreEntities());
+        BrandRepository repoBrand = new BrandRepository(new Areas.Panel.Models.goldstoreEntities());
         // GET: Shop
         public ActionResult Index()
         {
             return RedirectToAction("Products");
+        }
+        public PartialViewResult PartialNewArrivals ()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult PartialBrands()
+        {
+
+            return PartialView(repoBrand.GetAll());
         }
         public ActionResult PartialCategory()
         {
@@ -22,25 +38,139 @@ namespace goldStore.Controllers
 
         }
 
-        public ActionResult Products()
-
+        public ActionResult PartialPrice()
         {
+            return PartialView();
 
-            return View(repoProduct.GetAll());
         }
 
-        public ActionResult Product(int ? categoryId)
+      
+        public ActionResult Products (int?brandId,  int? categoryId, decimal? max, decimal? min, int?  page, int? PageSize, int? orderBy)
         {
-
-            var result = repoProduct.GetAll();
-            if(categoryId!=null)
+            ViewBag.orderBy = new List<SelectListItem>()
             {
 
-                result = result.Where(x => x.categoryId == categoryId).ToList();
+
+                new SelectListItem{ Text ="Fiyat", Value="1",Selected=true},
+                 new SelectListItem{ Text ="İsim", Value="2"}
+
+            };
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+
+
+                new SelectListItem{ Text ="20", Value="20",Selected=true},
+                 new SelectListItem{ Text ="10", Value="10"},
+                   new SelectListItem{ Text ="5", Value="5"},
+                     new SelectListItem{ Text ="2", Value="2"},
+                       new SelectListItem{ Text ="1", Value="1"}
+
+            };
+
+
+
+            int _page = page ?? 1;
+            int _pageSize = PageSize ?? 6;
+            TempData["PageSize"] = _pageSize;
+            TempData.Keep("PageSize");
+
+            var result = repoProduct.GetAll();
+
+
+            {
+              if (brandId != null)
+                {
+                    result = result.Where(x => x.brandId == brandId).ToList();
+                }
+
+                else if (categoryId != null)
+                {
+                    result = result.Where(x => x.categoryId == categoryId).ToList();
+                }
+                else if (max != null && min != null)
+                {
+                    result = result.Where(x => x.price >= min && x.price <= max).ToList();
+                }
+
+
+                else if (orderBy ==1)
+                {
+
+                    result = result.OrderBy(x => x.price).ToList();
+
+                }
+                else if (orderBy == 2)
+                {
+
+                    result = result.OrderBy(x => x.productName).ToList();
+
+                }
+
+                return View(result.ToPagedList(_page, _pageSize));
             }
 
-            return View(result);
+           
+        }
+           
+        public ActionResult ProductDetail (int productId)
+        {
+
+            return View(repoProduct.Get(productId));
+        }
+        public ActionResult Thumbnail(int width, int height, int Id, int _imageId)
+        {
+            byte[] photo = repoProduct.Get(Id).productImage.FirstOrDefault(x=>x.imageId==_imageId).image;
+            var base64 = Convert.ToBase64String(photo);
+            // Convert Base64 String to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+
+            using (var newImage = new Bitmap(width, height))
+            using (var graphics = Graphics.FromImage(newImage))
+            using (var stream = new MemoryStream())
+            {
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphics.DrawImage(image, new Rectangle(0, 0, width, height));
+                newImage.Save(stream, ImageFormat.Png);
+                return File(stream.ToArray(), "image/png");
+            }
 
         }
+
+        public ActionResult BrandThumbnail(int width, int height, int Id, int _imageId)
+        {
+            byte[] photo = repoBrand.Get(Id).image;
+            var base64 = Convert.ToBase64String(photo);
+            // Convert Base64 String to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+
+            using (var newImage = new Bitmap(width, height))
+            using (var graphics = Graphics.FromImage(newImage))
+            using (var stream = new MemoryStream())
+            {
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphics.DrawImage(image, new Rectangle(0, 0, width, height));
+                newImage.Save(stream, ImageFormat.Png);
+                return File(stream.ToArray(), "image/png");
+            }
+
+        }
+
     }
-}
+
+
+       
+           
+        }
+   
--- goldStore/asset/js/main.js
+++ goldStore/asset/js/main.js
@@ -336,6 +336,8 @@
           values: [110, 400],
           slide: function(event, ui) {
               $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+              $("#min").val(ui.values[0]);
+              $("#max").val(ui.values[1]);
           }
     });
     $("#amount").val("$" + $("#slider-range").slider("values", 0) +
